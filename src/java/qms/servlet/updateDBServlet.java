f0e14613be94315168efc78b4db1b949f45d2c76
hanryham <hamhanry@gmail.com>
2015-02-09 12:57:59 +0700

    1 array
    1 array

--- src/java/qms/servlet/updateDBServlet.java
+++ src/java/qms/servlet/updateDBServlet.java
@@ -76,35 +76,47 @@ public class updateDBServlet extends HttpServlet {
          
          //For count how many questions in the campaign
          int questionCounter = Integer.parseInt(request.getParameter("questionCounter"));
+         String questionType = request.getParameter("questionTypeInput");
+         //System.out.println("qustionType : " + request.getParameter("questionTypeInput")); 
+         String [] tmpQuestionType = questionType.split(",");
          
+         //TEXT
+         int textHelp=0;
          int questionNo = Integer.parseInt(request.getParameter("textCounterInput"));
          String questionText = request.getParameter("questionTextInput");
          String [] tmpQuestionText = questionText.split(",");
          String questionDesc = request.getParameter("questionDescInput");;
          String [] tmpQuestionDesc = questionDesc.split(",");
          
-         /*for(int i=0; i<sectionNo; i++){
-             System.out.println("sectionTitle : "+tmpSectionTitle[i]);
-         }*/
+         //PARAGRAPH
+         int paraHelp=0;
+         System.out.println("PARAGRAPH NO : "+request.getParameter("paraCounter"));
+         int paragraphNo = Integer.parseInt(request.getParameter("paraCounter"));
+         String paragraphText = request.getParameter("paragraphQuestionInput");
+         String [] tmpParagraphText = paragraphText.split(",");
+         String paragraphDesc = request.getParameter("paragraphDescInput");;
+         String [] tmpParagraphDesc = paragraphDesc.split(",");
          
-         
-         
-         
-         
-         
-         
-         
-         //System.out.println(id + ":" + name + ":" + lastname);
          System.out.println("campaign name : "+campaignName);
          System.out.println("Start date : "+ startDate);
          System.out.println("End date : "+ endDate);
-         System.out.println("questionTitle : "+questionTitle);
-         System.out.println("questionDescription : "+questionDescription);
          
+         for(int i=0; i<sectionNo; i++){
+             System.out.println("sectionTitle"+i+" : "+tmpSectionTitle[i]);
+             System.out.println("sectionText : "+tmpSectionText[i]);
+         }
          
-         /*System.out.println("questionNo : "+questionNo);
-         System.out.println("questionText : "+questionText);
-         System.out.println("questionDesc : "+questionDesc);*/
+         System.out.println("questionNo : "+questionNo);
+         
+         for(int i=0; i<questionNo; i++){
+             System.out.println("questionText :"+i+" :  "+tmpQuestionText[i]);
+             System.out.println("questionDesc :"+i+" :  "+tmpQuestionDesc[i]);
+         }
+         
+         for(int i=0; i<paragraphNo; i++){
+             System.out.println("paragraphText :"+i+" :  "+tmpParagraphText[i]);
+             System.out.println("paragraphDesc :"+i+" :  "+tmpParagraphDesc[i]);
+         }
          
          
          connection con = new connection();
@@ -129,7 +141,7 @@ public class updateDBServlet extends HttpServlet {
             //Logger.getLogger(scanData.class.getName()).log(Level.SEVERE, null, ex);
         }    
          
-         //INSERT SECTION TITLE & DESCRIPTION
+        //INSERT SECTION TITLE & DESCRIPTION
         query="select a.ID from questionnaire a where a.Title='"+questionTitle+"'";
         try {
            ResultSet rs = con.execQuery(query);            
@@ -146,20 +158,44 @@ public class updateDBServlet extends HttpServlet {
        }
          
         
-         /*
+        //--START FROM THIS ONE 
         //INSERT QUESTION
-        query="select a.ID from section a where a.Title='"+questionTitle+"'";
-         try {
-            ResultSet rs = con.execQuery(query);            
-            if(rs.next()){
-                query = "insert into section(QuestionnaireID,Title, Text) values("+Integer.parseInt(rs.getString(1))+",'"+sectionTitle+"','"+sectionText+"')";
-                con.execUpdate(query);
-            }else{
-                out.print("wrong");
+        System.out.println("adhaskdaskdksahdks : "+questionCounter);
+        for(int i=0;i<sectionNo;i++){
+            query="select ID from section "
+                    + "where Title='"+tmpSectionTitle[i]+"'";
+            try {
+                ResultSet rs = con.execQuery(query); 
+                paraHelp=0;
+                textHelp=0;
+                if(rs.next()){
+                    for(int j=0;j<questionCounter;j++){
+                        if(Integer.parseInt(tmpQuestionType[j])==1){//TEXT
+                            query = "insert into question(QuestionTypeID,SectionID,Text)"
+                                    + "values("+Integer.parseInt(tmpQuestionType[j])+", "+Integer.parseInt(rs.getString(1))+", "
+                                    + "'"+tmpQuestionText[textHelp]+"')";
+                            textHelp++;
+                        //}else if(Integer.parseInt(tmpQuestionType[j])==18){//GRID
+                        //}else if(Integer.parseInt(tmpQuestionType[j])==5){//MULTIPLE CHOICE
+                        }else if(Integer.parseInt(tmpQuestionType[j])==12){//PARAGRAPH
+                            System.out.println(tmpParagraphText[paraHelp]);
+                            query = "insert into question(QuestionTypeID,SectionID,Text)"
+                                    + "values("+Integer.parseInt(tmpQuestionType[j])+", "+Integer.parseInt(rs.getString(1))+", "
+                                    + "'"+tmpParagraphText[paraHelp]+"')";
+                            paraHelp++;
+                            
+                        }else{
+                            
+                        }
+                        con.execUpdate(query);                        
+                    }
+                }else{
+                    out.print("wrong");
+                }
+            } catch (SQLException ex) {
+                //Logger.getLogger(scanData.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (SQLException ex) {
-            //Logger.getLogger(scanData.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }             
                 
         //SHOW QUERY
         // query =  "select * from campaign";
@@ -170,7 +206,7 @@ public class updateDBServlet extends HttpServlet {
         //    }
         // } catch (SQLException ex) {
         //    Logger.getLogger(updateDBServlet.class.getName()).log(Level.SEVERE, null, ex);
-        //}*/
+        //}
          
          con.closeConnect();
          
